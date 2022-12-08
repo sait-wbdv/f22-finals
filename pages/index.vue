8d0b67aa34346807634da0da82239fbb65eb2a0b
Tony Grimes <acidtone@tonygrimes.com>
2022-12-08 12:35:35 -0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,263 @@
+<template>
+  <div class="page-wrapper">
+    <header>
+      <img src="/images/sait-logo.png" alt="SAIT Logo">
+      <div>
+        <h1>SAIT Web Developer Fast Track</h1>
+        <h2>Final Presentations - April 22, 2022</h2>
+      </div>
+    </header>
+    <main>
+      <h3>Introductions</h3>
+      <ul class="overview">
+        <li><a href="https://www.linkedin.com/in/tony-grimes-778ba135/">Tony Grimes</a>: Lead Instructor</li>
+        <li><a href="https://www.linkedin.com/in/ashlyn-knox13/">Ashlyn Knox</a>: Co-Instructor</li>
+        <li><a href="https://www.linkedin.com/in/dasachadwick/">Dasa Chadwick</a>: Career and Consulting Instructor</li>
+        <li>Facilitators:
+          <ul>
+            <li>Jess</li>
+            <li>Patrick</li>
+            <li>Mike</li>
+            <li>Fesal</li>
+          </ul>
+        </li>
+        <li><a href="https://www.linkedin.com/in/autumn-heinze-48624a50/">Autumn Heinze</a>: Work Integrated Learning Coordinator</li>
+        <li><a href="https://www.linkedin.com/in/eringates28/">Erin Gates</a>: Academic Chair, School of Advanced Digital Technology</li>
+      </ul>
+      <h3>Program Overview</h3>
+      <ul class="overview">
+        <li><em>Tools</em>: Git/npm, VS Code, Postman</li>
+        <li><em>Development</em>: HTML/CSS/JS, VueJS/NuxtJS, StoryBlok, CSS Tailwind</li>
+        <li><em>Deployment</em>: GitHub, Netlify</li>
+        <li><em>Design</em>: Figma, Photoshop/GIMP</li>
+      </ul>
+      <h3 class="rounds open">10:15am: Round 1 Presentations</h3>
+      <section id="round1">
+        <!-- Dynamic Content Here-->
+      </section>
+      <h3 class="rounds">11:30am: Round 2 Presentations</h3>
+      <section id="round2">
+        <!-- Dynamic Content Here-->
+      </section>
+      <h3 class="rounds">End of Presentations!</h3>
+    </main>
+  </div>
+</template>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Raleway:wght@300&display=swap');
+/* Font */
+body {
+  font-family: 'Open Sans', sans-serif;
+  font-weight: 300;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: 'Raleway', sans-serif;
+  font-weight: 300;
+}
+
+
+body {
+  font-size: 20px;
+  margin-bottom: 15rem;
+}
+
+h1, h2, h3, h4, h5, h6, p {
+  margin: 0;
+  line-height: 1;
+  text-align: center;
+}
+
+li {
+  margin: 0;
+  line-height: 1;
+}
+
+ul {
+  list-style-type: none;
+  padding-left: 0;
+}
+
+.page-wrapper {
+  max-width: 70ch;
+  margin: auto;
+  padding: 1rem;
+}
+
+.page-wrapper > header {
+  display: flex;
+  align-items: center;
+
+  border-bottom: 1px solid #aaa;
+  padding: 1em;
+  margin-bottom: 2em;
+}
+
+.page-wrapper > header img {
+  width: 150px;
+}
+
+.page-wrapper > header div {
+  flex-grow: 1;
+}
+
+.page-wrapper > header h1,
+.page-wrapper > header h2 {
+  margin: 0;
+  text-align: center;
+}
+
+main {
+  margin: auto;
+  max-width: 62ch;
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+main > h3 {
+  font-size: 3rem;
+  width: max-content;
+  margin: 7rem 5rem auto;
+  color: rebeccapurple;
+  font-variant: small-caps;
+}
+
+.overview {
+  text-align: left;
+}
+.overview li {
+  font-size: 1.5rem;
+  line-height: 1.5;
+  list-style: initial;
+}
+
+.overview ul {
+  padding-left: 2.5rem;
+}
+
+.presentation {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: 5rem auto;
+}
+.presentation header {
+  margin-bottom: 2rem;
+  text-align: center;
+  color: #666;
+  margin: 0;
+  width: 100%;
+}
+
+.presentation header h4 {
+  font-size: 3rem;
+  margin: 1rem;
+}
+
+.presentation header h5 {
+  font-style: italic;
+  font-size: 1.5rem;
+}
+
+figure {
+  display: grid;
+  grid-template-columns: 12em 1fr;
+  grid-template-rows: 1fr;
+  
+  align-items: center;
+  justify-content: center;
+
+  min-width: 50ch;
+  min-height: 12em;
+  padding: 1rem;
+
+  border: 1px solid #bbb;
+  border-radius: 4rem;
+  background-image: radial-gradient(skyblue 30%, white 30.1%);
+  background-size: 100em 100em;
+  background-position: 52rem;
+}
+
+
+figure > img {
+  grid-column: 1 / 2;
+  grid-row: 1 / -1;
+  
+  width: 8em;
+  height: 8em;
+  margin: auto;
+  object-fit: cover;
+
+  border-radius: 50%;
+}
+
+
+.badges img {
+  width: 2.25rem;
+  margin: 0 0.7rem;
+}
+
+figcaption {
+  text-align: center;
+  margin: 0;
+  padding: 0 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+
+figcaption header {
+  font-size: 2.5rem;
+  line-height: 1.2;
+  color: #666;
+  width: max-content;
+  margin-top: .5rem;
+  text-align: center;
+}
+
+figcaption p {
+  margin: 1.25rem auto;
+  margin-bottom: 0.5rem;
+  line-height: 1.5;
+}
+
+.social ul, .badges ul {
+  display: flex;
+  justify-content: center;
+  margin: 1.25rem 0;
+}
+
+.badges ul {
+  margin-bottom: 0.75rem;
+}
+
+.social li {
+  font-size: 2rem;
+  margin: 0.15rem 0.75rem;
+
+}
+.social li a {
+  color: navy;
+}
+
+.group figure > img {
+  width: 7em;
+  height: 7em;
+}
+
+.group figure {
+  padding: 0 1rem;
+}
+
+.group figcaption p {
+  margin-bottom: 0.75rem;
+  margin: 0;
+}
+
+
+</style>
